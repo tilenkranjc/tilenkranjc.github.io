397b278410e71a87a70f8b4b7e1a480d395c7d15
Tilen Kranjc <tilen@tilenkranjc.com>
2019-10-06 21:43:54 +0200

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+// import Post from '../components/Post'
+import Sidebar from '../components/Sidebar'
+
+class IndexRoute extends React.Component {
+  render() {
+    // const items = []
+    const { title, subtitle } = this.props.data.site.siteMetadata
+    const page = this.props.data.markdownRemark
+    // const posts = this.props.data.allMarkdownRemark.edges
+    /* posts.forEach(post => {
+      items.push(<Post data={post} key={post.node.fields.slug} />)
+    }) */
+
+    return (
+      <Layout>
+        <div>
+          <Helmet>
+            <title>{title}</title>
+            <meta name="description" content={subtitle} />
+          </Helmet>
+          <Sidebar {...this.props} />
+          <div className="content">
+            <div className="content__inner">
+              <div className="page">
+                <h1 className="page__title">{page.frontmatter.title}</h1>
+                <div
+                  className="page__body"
+                /* eslint-disable-next-line react/no-danger */
+                  dangerouslySetInnerHTML={{ __html: page.html }}
+                />
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default IndexRoute
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        menu {
+          label
+          path
+        }
+        author {
+          name
+          email
+          telegram
+          twitter
+          github
+          rss
+          vk
+        }
+      }
+    }
+    markdownRemark(fileAbsolutePath: { regex: "/index.md/" }) {
+      id
+      html
+      frontmatter {
+        title
+        date
+        description
+      }
+    }
+  }
+`
---
+++ src/pages/index.md
@@ -0,0 +1,12 @@
+---
+title: "Hello world"
+layout: page
+path: "/"
+---
+<img src="profilka_small.jpg">
+
+<p>Welcome to my website.</p>
+<p>I am a bioinformatician, an entrepreneur and a coder. My day job is at Biosistemika, my night job is freelancing, my free time job is being a dad. </p>
+<p>Data science, visualisation and communication has been always my passion. You can find some examples in my <a href="posters_figures.html">posters and figures</a>. 
+<p>I particularly enjoy <a href="research.html">analysis</a> of biological data. My speciality is biological and medical image analysis. </p>
+<p>If you feel you could use my help, feel free to <a href="contact.html">get in touch</a>!</p>
